1e480e89be1d2de6293259da4881403dd4211caf
habii45 <habyl5430@gmail.com>
2026-05-31 21:55:22 +0000

    ajout du bouton archver

--- scripts.js
+++ scripts.js
@@ -29,7 +29,9 @@ form.addEventListener("submit", (e) => {
 
     categorie: categoryInput.value,
 
-    description: descriptionInput.value
+    description: descriptionInput.value,
+
+    archivee: false
 
   }
 
@@ -69,6 +71,18 @@ function afficherIdees() {
     campus: "Vie de campus",
     technique: "Technique"
   }
+ const archiveBtn =
+document.getElementById("show-archives")
+
+const archives =
+idees.filter(
+  idee => idee.archivee
+)
+
+archiveBtn.style.display =
+archives.length > 0
+? "inline-block"
+: "none"
 
   ideasContainer.innerHTML = ""
   if (idees.length === 0) {
@@ -99,7 +113,9 @@ function afficherIdees() {
 }
 
 
-  idees.forEach((idee) => {
+  idees
+   .filter(idee => !idee.archivee)
+  .forEach((idee) => {
 
     const badgeColor =
     couleurs[idee.categorie]
@@ -165,6 +181,14 @@ function afficherIdees() {
                   Supprimer
                 </a>
               </li>
+              <li>
+                 <a
+                 href="#"
+                 class="dropdown-item archive-btn"
+                 data-id="${idee.id}">
+                 Archiver
+               </a>
+             </li>
 
             </ul>
 
@@ -283,6 +307,126 @@ document
       document.getElementById("editModal")
     )
     .hide()
+
+})
+document.addEventListener("click", (e) => {
+
+  if (e.target.classList.contains("archive-btn")) {
+
+    e.preventDefault()
+
+    const id =
+    Number(e.target.dataset.id)
+
+    const idee =
+    idees.find(
+      idee => idee.id === id
+    )
+
+    idee.archivee = true
+
+    localStorage.setItem(
+      "idees",
+      JSON.stringify(idees)
+    )
+
+    afficherIdees()
+
+  }
+
+})
+
+document
+.getElementById("show-archives")
+.addEventListener("click", () => {
+    document.getElementById("show-archives").style.display = "none"
+
+    document.getElementById("show-ideas").style.display = "inline-block"
+
+  ideasContainer.innerHTML = `
+    <h3 class="mb-4">
+      Idées archivées
+    </h3>
+  `
+
+  const couleursCartes = {
+    pedagogie: "note-pedagogie",
+    evenement: "note-evenement",
+    campus: "note-campus",
+    technique: "note-technique"
+  }
+
+  const couleurs = {
+    pedagogie: "text-bg-warning",
+    evenement: "text-bg-info",
+    campus: "text-bg-secondary",
+    technique: "text-bg-success"
+  }
+
+  const labels = {
+    pedagogie: "Pédagogie",
+    evenement: "Événement",
+    campus: "Vie de campus",
+    technique: "Technique"
+  }
+
+  idees
+    .filter(idee => idee.archivee)
+    .forEach((idee) => {
+
+      const cardWrapper =
+      document.createElement("div")
+
+      cardWrapper.classList.add(
+        "col-12",
+        "col-md-6",
+        "col-lg-4"
+      )
+
+      const card =
+      document.createElement("div")
+
+      card.classList.add(
+        "card",
+        "border-0",
+        "shadow-sm",
+        "rounded-4",
+        couleursCartes[idee.categorie]
+      )
+
+      card.innerHTML = `
+        <div class="card-body">
+
+          <span class="badge rounded-pill ${couleurs[idee.categorie]}">
+            ${labels[idee.categorie]}
+          </span>
+
+          <h5 class="fw-bold mt-3">
+            ${idee.titre}
+          </h5>
+
+          <p class="text-muted">
+            ${idee.description}
+          </p>
+
+        </div>
+      `
+
+      cardWrapper.appendChild(card)
+      ideasContainer.appendChild(cardWrapper)
+
+    })
+
+})
+document
+.getElementById("show-ideas")
+.addEventListener("click", () => {
+
+  afficherIdees()
+
+  document.getElementById("show-ideas").style.display = "none"
+
+  document.getElementById("show-archives").style.display = "inline-block"
 
 })
 
